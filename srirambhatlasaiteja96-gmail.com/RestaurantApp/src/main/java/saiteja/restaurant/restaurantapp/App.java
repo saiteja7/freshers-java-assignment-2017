332db464421521248818f97c5f6623115ec2ecd1
saiteja7 <srirambhatlasaiteja96@gmail.com>
2017-09-09 20:59:12 +0530

    Add files via upload

---
+++ srirambhatlasaiteja96-gmail.com/RestaurantApp/src/main/java/saiteja/restaurant/restaurantapp/App.java
@@ -0,0 +1,20 @@
+package saiteja.restaurant.restaurantapp;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+
+{
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+       CustomerWatchmen customerWatchmen = new CustomerWatchmen();
+       customerWatchmen.startConv();
+       
+       
+      
+    }
+}
---
+++ srirambhatlasaiteja96-gmail.com/RestaurantApp/src/main/java/saiteja/restaurant/restaurantapp/TakeAway.java
@@ -0,0 +1,7 @@
+package saiteja.restaurant.restaurantapp;
+
+public class TakeAway {
+    public void customerArraived(){
+        System.out.println("This is take away");
+    }
+}
